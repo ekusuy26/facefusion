e0fe8ac58c522fcaae01a84e2fb2e091761d4713
ekusuy26 <y.yuu6221@gmail.com>
2020-05-02 19:03:33 +0900

    Processing of the transmitted image is completed

--- myhp/views.py
+++ myhp/views.py
@@ -1,30 +1,34 @@
+from django.http import HttpResponse
 from django.shortcuts import render, redirect
 from .forms import DocumentForm
 from .models import Document
-from PIL import Image, ImageFilter
+from PIL import Image
+from django.conf import settings
 import cv2
- 
  
 def index(request):
     if request.method == 'POST':
         form = DocumentForm(request.POST, request.FILES)
         if form.is_valid():
-            img_moto =Image.open(request.FILES['photo'].name)
-            src = cv2.imread(img_moto)
-            def mosaic(src, ratio=0.1):
-                small = cv2.resize(src, None, fx=ratio, fy=ratio, interpolation=cv2.INTER_NEAREST)
-                return cv2.resize(small, src.shape[:2][::-1], interpolation=cv2.INTER_NEAREST)
-            
-            dst_01 = mosaic(src)
-            cv2.imwrite('./media/documents/opencv_mosaic_01.jpg', dst_01)
-
             form.save()
             return redirect('myhp:index')
     else:
         form = DocumentForm()
-        obj = Document.objects.all()
+        max_id = Document.objects.latest('id').id
+        obj = Document.objects.get(id = max_id)
+        input_path = settings.BASE_DIR + obj.photo.url
+        output_path = settings.BASE_DIR + "/media/mosaics/output.jpg"
+        gray(input_path,output_path)
  
     return render(request, 'index.html', {
         'form': form,
         'obj': obj
     })
+
+def gray(input_path,output_path):
+    src = cv2.imread(input_path)
+    def mosaic(src, ratio=0.1):
+        small = cv2.resize(src, None, fx=ratio, fy=ratio, interpolation=cv2.INTER_NEAREST)
+        return cv2.resize(small, src.shape[:2][::-1], interpolation=cv2.INTER_NEAREST)
+    dst_01 = mosaic(src)
+    cv2.imwrite(output_path, dst_01)
