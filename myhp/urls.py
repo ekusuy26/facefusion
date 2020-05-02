be71ad0df08115b4bab44bf0abb1bf57dd59dc31
ekusuy26 <y.yuu6221@gmail.com>
2020-05-02 13:52:17 +0900

    Implemented image upload function

--- myhp/urls.py
+++ myhp/urls.py
@@ -5,4 +5,5 @@ app_name='myhp'
  
 urlpatterns = [
     path('', views.index, name='index'),
+    path('model_form_upload/', views.index, name='index'),
 ]
--- myhp/views.py
+++ myhp/views.py
@@ -1,5 +1,19 @@
-from django.shortcuts import render
-
-# Create your views here. 
+from django.shortcuts import render, redirect
+from .forms import DocumentForm
+from .models import Document
+ 
+ 
 def index(request):
-    return render(request,'index.html')
+    if request.method == 'POST':
+        form = DocumentForm(request.POST, request.FILES)
+        if form.is_valid():
+            form.save()
+            return redirect('myhp:index')
+    else:
+        form = DocumentForm()
+        obj = Document.objects.all()
+ 
+    return render(request, 'index.html', {
+        'form': form,
+        'obj': obj
+    })
