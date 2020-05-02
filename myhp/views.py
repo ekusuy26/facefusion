7081b172594fdb6fbde56859877dc5962f759f81
ekusuy26 <y.yuu6221@gmail.com>
2020-05-02 18:26:16 +0900

    Implementation of mosaic processing

--- myhp/views.py
+++ myhp/views.py
@@ -1,12 +1,23 @@
 from django.shortcuts import render, redirect
 from .forms import DocumentForm
 from .models import Document
+from PIL import Image, ImageFilter
+import cv2
  
  
 def index(request):
     if request.method == 'POST':
         form = DocumentForm(request.POST, request.FILES)
         if form.is_valid():
+            img_moto =Image.open(request.FILES['photo'].name)
+            src = cv2.imread(img_moto)
+            def mosaic(src, ratio=0.1):
+                small = cv2.resize(src, None, fx=ratio, fy=ratio, interpolation=cv2.INTER_NEAREST)
+                return cv2.resize(small, src.shape[:2][::-1], interpolation=cv2.INTER_NEAREST)
+            
+            dst_01 = mosaic(src)
+            cv2.imwrite('./media/documents/opencv_mosaic_01.jpg', dst_01)
+
             form.save()
             return redirect('myhp:index')
     else:
