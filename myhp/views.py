795e723a3590ca8c098e31c16ef58803d581ec40
ekusuy26 <y.yuu6221@gmail.com>
2020-05-03 15:58:50 +0900

    Face detection and mosaic

--- myhp/views.py
+++ myhp/views.py
@@ -30,8 +30,28 @@ def index(request):
 
 def gray(input_path,output_path):
     src = cv2.imread(input_path)
+
     def mosaic(src, ratio=0.1):
         small = cv2.resize(src, None, fx=ratio, fy=ratio, interpolation=cv2.INTER_NEAREST)
         return cv2.resize(small, src.shape[:2][::-1], interpolation=cv2.INTER_NEAREST)
     dst_01 = mosaic(src)
     cv2.imwrite(output_path, dst_01)
+
+    def mosaic_area(src, x, y, width, height, ratio=0.1):
+        dst = src.copy()
+        dst[y:y + height, x:x + width] = mosaic(dst[y:y + height, x:x + width], ratio)
+        return dst
+    
+    dst_area = mosaic_area(src, 100, 50, 100, 150)
+    
+    face_cascade_path = './haarcascade_frontalface_default.xml'
+    face_cascade = cv2.CascadeClassifier(face_cascade_path)
+    
+    src_gray = cv2.cvtColor(src, cv2.COLOR_BGR2GRAY)
+    
+    faces = face_cascade.detectMultiScale(src_gray)
+    
+    for x, y, w, h in faces:
+        dst_face = mosaic_area(src, x, y, w, h)
+    
+        cv2.imwrite(output_path, dst_face)
