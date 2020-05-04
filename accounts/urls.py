6318d920420a4492b1da612522964b76de653555
ekusuy26 <y.yuu6221@gmail.com>
2020-05-05 01:36:04 +0900

    Create links to individual user pages

--- accounts/urls.py
+++ accounts/urls.py
@@ -4,6 +4,6 @@ urlpatterns = [
     path('accounts/login/', views.MyLoginView.as_view(), name="login"),
     path('accounts/logout/', views.MyLogoutView.as_view(), name="logout"),
     path('accounts/create/', views.UserCreateView.as_view(),name="create"),
-    path('accounts/show/', views.show,name="show"),
+    path('accounts/show/<int:id>/', views.show,name="show"),
     path('index/',views.IndexView.as_view(), name="index")
 ]
