2818cbe96fe096f290476726d19980fd5100ea55
ekusuy26 <y.yuu6221@gmail.com>
2020-05-05 17:45:37 +0900

    I fixed the face detection, but an error occurs when the face detection fails.

--- myhp/views.py
+++ myhp/views.py
@@ -20,40 +20,39 @@ def index(request):
         input_path_two = settings.BASE_DIR + obj.photo_two.url
         output_path = settings.BASE_DIR + "/media/mosaics/output.jpg"
         output_path_two = settings.BASE_DIR + "/media/mosaics/output_two.jpg"
-        swap_faces(input_path,input_path_two,output_path,output_path_two)
- 
+
+        src = cv2.imread(input_path)
+        src_two = cv2.imread(input_path_two)
+        img = src.copy()
+        img_two = src_two.copy()
+
+        def mosaic_area(src, src_two, x, y, width, height, ratio=1):
+            dst = src.copy()
+            dst_02 = src_two.copy()
+            dst[y:y + height, x:x + width] = dst_02[y:y + height, x:x + width]
+            return dst
+    
+        face_cascade_path = './haarcascade_frontalface_default.xml'
+        face_cascade = cv2.CascadeClassifier(face_cascade_path)
+        face_cascade_two = cv2.CascadeClassifier(face_cascade_path)
+
+        src_gray = cv2.cvtColor(src, cv2.COLOR_BGR2GRAY)
+        img_gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+        src_two_gray = cv2.cvtColor(src_two, cv2.COLOR_BGR2GRAY)
+        img_two_gray = cv2.cvtColor(img_two, cv2.COLOR_BGR2GRAY)
+
+        faces = face_cascade.detectMultiScale(src_gray)
+        faces_two = face_cascade_two.detectMultiScale(img_two_gray)
+
+        for x, y, w, h in faces:
+            for a, b, c, d in faces_two:
+                dst_face_01 = mosaic_area(src, src_two, x, y, w, h)
+                dst_face_02 = mosaic_area(img_two, img, a, b, c, d)
+        
+                cv2.imwrite(output_path, dst_face_01)
+                cv2.imwrite(output_path_two, dst_face_02)
+
     return render(request, 'myhp/index.html', {
         'form': form,
         'obj': obj
     })
-
-def swap_faces(input_path,input_path_two,output_path,output_path_two):
-    src = cv2.imread(input_path)
-    src_two = cv2.imread(input_path_two)
-
-    def mosaic(src, ratio=1):
-        small = cv2.resize(src, None, fx=ratio, fy=ratio, interpolation=cv2.INTER_NEAREST)
-        return cv2.resize(small, src.shape[:2][::-1], interpolation=cv2.INTER_NEAREST)
-
-    def mosaic_area(src, src_two, x, y, width, height, ratio=1):
-        dst = src.copy()
-        dst_02 = src_two.copy()
-        dst[y:y + height, x:x + width] = mosaic(dst_02[y:y + height, x:x + width], ratio)
-        return dst
-
-    face_cascade_path = './haarcascade_frontalface_default.xml'
-    face_cascade = cv2.CascadeClassifier(face_cascade_path)
-    
-    src_gray = cv2.cvtColor(src, cv2.COLOR_BGR2GRAY)
-    src_two_gray = cv2.cvtColor(src_two, cv2.COLOR_BGR2GRAY)
-    
-    faces = face_cascade.detectMultiScale(src_gray)
-    faces_two = face_cascade.detectMultiScale(src_two_gray)
-    
-    for x, y, w, h in faces:
-        for a, b, c, d in faces_two:
-            dst_face_01 = mosaic_area(src, src_two, x, y, w, h)
-            dst_face_02 = mosaic_area(src_two, src, a, b, c, d)
-        
-            cv2.imwrite(output_path, dst_face_01)
-            cv2.imwrite(output_path_two, dst_face_02)
