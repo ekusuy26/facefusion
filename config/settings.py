3208f8c1f8a6812657b9aaa44605c03fc8dfc988
ekusuy26 <y.yuu6221@gmail.com>
2020-05-07 00:11:38 +0900

    a

--- config/settings.py
+++ config/settings.py
@@ -138,8 +138,8 @@ STATICFILES_DIRS = (
 # https://warehouse.python.org/project/whitenoise/
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
-MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+MEDIA_URL = '/media/'
 
 LOGIN_URL = 'login' # ログインしていないときのリダイレクト先
 LOGIN_REDIRECT_URL = '/' # ログイン後のリダイレクト先
