95673cbfa148d4cf881eedc1cd3c2f1879ade1fd
ekusuy26 <y.yuu6221@gmail.com>
2020-05-07 18:46:28 +0900

    message table migration

--- chat/models.py
+++ chat/models.py
@@ -1,3 +1,8 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
+class Chat(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    text = models.CharField(max_length=150)
+    uploaded_at = models.DateTimeField(auto_now_add=True)
