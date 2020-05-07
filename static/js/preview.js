594a49600021fa87c5842102e8941495443d297d
ekusuy26 <y.yuu6221@gmail.com>
2020-05-07 17:31:44 +0900

    Completion of layout of pet registration page

--- static/js/preview.js
+++ static/js/preview.js
@@ -27,3 +27,18 @@ $(document).on("change", "#id_photo_two", function(e) {
     return reader.readAsDataURL(e.target.files[0]);
   }
 });
+
+$(document).on("change", "#id_image", function(e) {
+  var reader;
+  if (e.target.files.length) {
+    reader = new FileReader;
+    reader.onload = function(e) {
+      var userThumbnail;
+      userThumbnail = document.getElementById('thumbnail');
+      $("#userImgPreview").addClass("is-active");
+      $("#thumbnail").addClass("is-dogImg");
+      userThumbnail.setAttribute('src', e.target.result);
+    };
+    return reader.readAsDataURL(e.target.files[0]);
+  }
+});
