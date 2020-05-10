33888a3333fe21f0e8205ccabd2e98a513d8b570
ekusuy26 <y.yuu6221@gmail.com>
2020-05-10 14:56:20 +0900

    Update settings.py

--- config/settings.py
+++ config/settings.py
@@ -158,6 +158,7 @@ if not DEBUG:
     AWS_SECRET_ACCESS_KEY = os.environ['AWS_SECRET_ACCESS_KEY']
     AWS_STORAGE_BUCKET_NAME = os.environ['AWS_STORAGE_BUCKET_NAME']
 
+    AWS_LOCATION = 'media'
     DEFAULT_FILE_STORAGE = 'storages.backends.s3boto.S3BotoStorage'
     S3_URL = 'http://%s.s3.amazonaws.com/' % AWS_STORAGE_BUCKET_NAME
     MEDIA_URL = S3_URL
