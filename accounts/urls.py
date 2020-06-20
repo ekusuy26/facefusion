f4ce1ff25fb944b10129dc6ac2ff8d23ff04c118
ekusuy26 <y.yuu6221@gmail.com>
2020-06-20 23:17:20 +0900

    a

--- accounts/urls.py
+++ accounts/urls.py
@@ -1,5 +1,6 @@
 from django.urls import path
 from . import views
+
 urlpatterns = [
     path('accounts/login/', views.MyLoginView.as_view(), name="login"),
     path('accounts/logout/', views.MyLogoutView.as_view(), name="logout"),
