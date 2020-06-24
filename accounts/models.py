1fc497d43ffa995e46c4ac7c085e6e6c811d2986
ekusuy26 <y.yuu6221@gmail.com>
2020-06-24 14:35:27 +0900

    Create like table to implement like function

--- accounts/models.py
+++ accounts/models.py
@@ -10,7 +10,12 @@ class Dog(models.Model):
     sex = models.CharField(max_length=100)
     introduction = models.CharField(max_length=255)
     image = models.ImageField(upload_to="dogs/")
+    like_num = models.IntegerField(default=0)
 
 class Crew(models.Model):
     users = models.ManyToManyField(User, verbose_name='ユーザー')
     name = models.CharField(max_length=20)
+
+class Like(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE, related_name='like_user')
+    dog = models.ForeignKey(Dog, on_delete=models.CASCADE)
