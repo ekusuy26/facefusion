d6eae2f3b34c214d778564c9c5c9d48fc4fd39f5
ekusuy26 <y.yuu6221@gmail.com>
2020-07-03 22:37:07 +0900

    Implement pagination in result as well

--- myhp/views.py
+++ myhp/views.py
@@ -57,7 +57,10 @@ def show(request):
     })
 
 def result(request):
-    objs = Document.objects.all()
+    all_objs = Document.objects.order_by('-id')
+    paginator = Paginator(all_objs, 10)
+    p = request.GET.get('p')
+    objs = paginator.get_page(p)
     return render(request, 'myhp/result.html', {
         'objs': objs
     })
